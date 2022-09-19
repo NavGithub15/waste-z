27ef42d66d0f54181ab21ce01089b3b89da467e1
Naveen Sharma <103960076+NavGithub15@users.noreply.github.com>
2022-09-19 14:59:18 -0400

    Add hero image and style it for all breakpoints, add hero component to homepage

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 
+import Hero from "../../components/Hero/Hero";
 import "./HomePage.scss";
 
 export default function HomePage() {
   return (
     <>
+    <Hero />
     
     
     </>
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import "./Hero.scss";
+
+export default function Hero() {
+  return (
+    <section className="hero-wrapper">
+        <div className="hero">
+            <h1 className="hero__title">Waste <strong>Z</strong></h1>
+            <h3 className="hero__subtitle">Smarter way to track your food in the kitchen</h3>
+            <p className="hero__text">Prevent food waste before it happens</p>
+        </div>
+
+    </section>
+  )
+}
