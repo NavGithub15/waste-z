007f780c71baa48b09d4743c4aa3a392bba5e44e
Naveen Sharma <103960076+NavGithub15@users.noreply.github.com>
2022-09-20 16:38:04 -0400

    Add firebase authentication and fix the api issues

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -1,22 +1,65 @@
 import Inventory from "../../Pages/Inventory/Inventory";
 import "./Authentication.scss";
+import { auth } from "../../firebase.config";
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Authentication() {
 
-    const [registerUsername, setRegisterUsername] = useState("");
-    const [registerName, setRegisterName] = useState("");
-    const [registerPassword, setRegisterPassword] = useState("");
-    const [registerConfirmPassword, setRegisterConfirmPassword] = useState("");
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [error, setError] = useState("");
+  const [errorLogin, setErrorLogin] = useState(false);
+  const [loginErrorMessage, setLoginErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
 
     const handleSignup = (e) => {
         e.preventDefault();
+        
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const confirmPassword = e.target.confirmPassword.value;
+
+        if (password !== confirmPassword) {
+          return setError("Passwords do not match");
+        }
+
+           createUserWithEmailAndPassword(auth, email, password, confirmPassword)
+           .then((cred) => {
+            setIsSignedUp(true);
+            navigate("/inventory");
+           })
+           .catch ((err) => {
+          setError("Failed to sign up! Please try again");
+        })
 
     }
 
     const handleLogin = (e) => {
         e.preventDefault();
-        
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+          setIsLoggedIn(true);
+        })
+        .catch((err) => {
+          setErrorLogin("Failed to login! Please try again")
+        });
+    }
+
+    const logout = () => {
+      return auth.signOut();
+
     }
 
     //   Sign up render
@@ -25,13 +68,13 @@ export default function Authentication() {
           <h1>Sign Up</h1>
           <form onSubmit={handleSignup}>
             <div className="form-group">
-              Username: <input type="text" name="username" />
-            </div>
-            <div className="form-group">
-              Name: <input type="text" name="name" />
+              Email: <input type="text" name="email" />
             </div>
             <div className="form-group">
               Password: <input type="password" name="password" />
+            </div>
+            <div className="form-group">
+              Confirm Password: <input type="password" name="confirmPassword" />
             </div>
             <button type="submit" className="btn btn-primary">
               Signup
@@ -47,7 +90,8 @@ export default function Authentication() {
           {/* {isLoginError && <label className="error">{errorMessage}</label>} */}
           <form onSubmit={handleLogin}>
             <div className="form-group">
-              Username: <input type="text" name="username" />
+            {errorLogin && <label className="error">{loginErrorMessage}</label>}
+              Email: <input type="text" name="email" />
             </div>
             <div className="form-group">
               Password: <input type="password" name="password" />
@@ -59,9 +103,9 @@ export default function Authentication() {
         </div>
       );
 
-      const logout = () => {
-
-      }
+        // Handle the Signup/Login
+  if (!isSignedUp) return renderSignUp();
+  if (!isLoggedIn) return renderLogin();
 
 
   return (
