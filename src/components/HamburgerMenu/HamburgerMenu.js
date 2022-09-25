7094e30c68e2fb9990a08311ee48dc4ab46f59ec
Naveen Sharma <103960076+NavGithub15@users.noreply.github.com>
2022-09-25 16:03:36 -0400

    Fix burger menu with react burger library and fix all the links associated with burger menu

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -2,29 +2,28 @@ import "./HamburgerMenu.scss";
 import { NavLink } from "react-router-dom";
 import { auth } from "../../firebase.config";
 import { useAuthState } from "react-firebase-hooks/auth"
+import { slide as Menu } from 'react-burger-menu';
+import { useState } from "react";
 
-export default function HamburgerMenu() {
+export default function HamburgerMenu ({styles}) {
 
-  const [currentUser] = useAuthState (auth)
+  const [currentUser] = useAuthState(auth)
+
+  // const handleOnClose = (e) => {
+  //   e.preventDefault();
+  // }
 
   return (
-    <div className="menu">
-      <div className="menu__toggle">
-        <input className="menu__input" type="checkbox" />
-        <span className="menu__burger"></span>
-        <span className="menu__burger"></span>
-        <span className="menu__burger"></span>
-        <ul className="menu__lists">
-          <div className="menu__links-container">
-          <NavLink to="/" className="menu__link">Home</NavLink>
-          {!currentUser ? (<NavLink to="/signIn" className="menu__link">My Storage</NavLink>) : 
-                  (<NavLink to="/myStorage" className="menu__link">My Storage</NavLink>
-                  )}
-            <NavLink className="menu__link" to="/signUp">Get Started</NavLink>
-            <NavLink className="menu__link" to="/donate">Donate</NavLink>
-          </div>
-        </ul>
-      </div>
-    </div>
+    <Menu styles={styles}>
+      <NavLink to="/" className="menu__link" end>Home</NavLink>
+      {!currentUser ? (<NavLink to="/signIn" className="menu__link">My Storage</NavLink>) :
+        (<NavLink to="/myStorage" className="menu__link">My Storage</NavLink>
+        )}
+      {!currentUser ? (<NavLink className="menu__link" to="/signUp">Get Started</NavLink>) :
+        (<NavLink to="/recipe" className="menu__link">Recipes</NavLink>
+        )}
+        <NavLink className="menu__link" to="/storage">Storage</NavLink>
+      <NavLink className="menu__link" to="/donate">Donate</NavLink>
+    </Menu>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,30 @@
 import "./Header.scss";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
-import { Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { auth } from "../../firebase.config";
 import { useAuthState } from "react-firebase-hooks/auth"
 
 export default function Header() {
 
-  const [currentUser] = useAuthState (auth)
+  const [currentUser] = useAuthState(auth)
 
-  return(
+  return (
 
     <header className="header">
       <HamburgerMenu />
-        <div className="header__container">
-            <Link to="/" className="header__logo-link">Waste <strong>Z</strong></Link>
-              <nav className="header__links-container">
-                  <NavLink to="/" end className="header__link">Home</NavLink>
-                  {!currentUser ? (<NavLink to="/signIn" className="header__link">My Storage</NavLink>) : 
-                  (<NavLink to="/myStorage" className="header__link">My Storage</NavLink>
-                  )}
-                  <NavLink to="/signUp" className="header__link">Get Started</NavLink>
-              </nav>
-        </div>
+      <div className="header__container">
+        <Link to="/" className="header__logo-link">Waste <strong>Z</strong></Link>
+        <nav className="header__links-container">
+          <NavLink to="/" end className="header__link">Home</NavLink>
+          {!currentUser ? (<NavLink to="/signIn" className="header__link">My Storage</NavLink>) :
+            (<NavLink to="/myStorage" className="header__link">My Storage</NavLink>
+            )}
+          {!currentUser ? (<NavLink className="header__link" to="/signUp">Get Started</NavLink>) :
+            (<NavLink to="/recipe" className="header__link">Recipes</NavLink>
+            )}
+          <NavLink to="/donate" className="header__link">Donate</NavLink>
+        </nav>
+      </div>
     </header>
   )
 };
