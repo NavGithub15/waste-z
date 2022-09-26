571db7fd3a537dd015b81bc66b399f0c8e773644
Naveen Sharma <103960076+NavGithub15@users.noreply.github.com>
2022-09-25 22:07:33 -0400

    Add styles to storage cards

--- src/Pages/Storage/Storage.js
+++ src/Pages/Storage/Storage.js
@@ -3,7 +3,6 @@ import { vegetables, dairy, fruits, meats, condiments, pantry, veganProteins } f
 import { useState, useEffect } from "react";
 
 export default function Storage() {
-  // const [items, setItems] = useState([]);
   const [dairyItems, setDairyItems] = useState([]);
   const [vegetablesItems, setVegetablesItems] = useState([]);
   const [fruitsItems, setFruitsItems] = useState([]);
@@ -12,7 +11,6 @@ export default function Storage() {
   const [pantryItems, setPantryItems] = useState([]);
   const [veganProteinsItems, setVeganProteinsItems] = useState([]);
 
-
   useEffect(() => {
     vegetables().then(response => setVegetablesItems(response))
     dairy().then(response => setDairyItems(response))
@@ -20,66 +18,66 @@ export default function Storage() {
     meats().then(response => setMeatsItems(response))
     condiments().then(response => setCondimentsItems(response))
     pantry().then(response => setPantryItems(response))
-    veganProteins().then(response => setVeganProteinsItems(response))
+    veganProteins().then(response => setVeganProteinsItems(response))   
   }, []);
-
 
   return (
 
     <section className="storage__card card">
+      <h1 className="card-title">Storage Guide</h1>
       <div className="card__container">
-        <h2 className="card__heading">Dairy & Eggs</h2>
         {dairyItems.map((item) => {
           return (
-            <div key={item.id} className="card__content-wrapper">
-              {" "}
-              <div className="card__name-wrapper">
-                <h3 className="card__name">{item.name}</h3>
-              </div>
-              <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
-                <p className="card__fresh">{item.fresh}</p>
-              </div>
-              <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
-                <p className="card__fridge">{item.fridge}</p>
-              </div>
-              <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
-                <p className="card__freezer">{item.freezer}</p>
-              </div>
-              <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
-                <p className="card__storage">{item.storage}</p>
-              </div>
+          <div key={item.id} className="card__content-wrapper">
+            <h2 className="card__heading">Dairy & eggs</h2>
+            <div className="card__name-wrapper">
+              <h3 className="card__name">{item.name}</h3>
             </div>
+            <div className="card__fresh-wrapper">
+              <h3 className="card__content-heading">At Freshest</h3>
+              <p className="card__fresh">{item.fresh}</p>
+            </div>
+            <div className="card__fridge-wrapper">
+              <h3 className="card__content-heading">Refrigerator</h3>
+              <p className="card__fridge">{item.fridge}</p>
+            </div>
+            <div className="card__freezer-wrapper">
+              <h3 className="card__content-heading">Freezing</h3>
+              <p className="card__freezer">{item.freezer}</p>
+            </div>
+            <div className="card__storage-wrapper">
+              <h3 className="card__content-heading">Storage Guide</h3>
+              <p className="card__storage">{item.storage}</p>
+            </div>
+          </div>
           );
         })}
       </div>
 
       <div className="card__container">
-        <h2 className="card__heading">Vegetables</h2>
-            {vegetablesItems.map((item) => {
+        
+        {vegetablesItems.map((item) => {
           return (
             <div key={item.id} className="card__content-wrapper">
               {" "}
+              <h2 className="card__heading">Vegetables</h2>
               <div className="card__name-wrapper">
                 <h3 className="card__name">{item.name}</h3>
               </div>
               <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
+                <h3 className="card__content-heading">At Freshest</h3>
                 <p className="card__fresh">{item.fresh}</p>
               </div>
               <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
+                <h3 className="card__content-heading">Refrigerator</h3>
                 <p className="card__fridge">{item.fridge}</p>
               </div>
               <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
+                <h3 className="card__content-heading">Freezing</h3>
                 <p className="card__freezer">{item.freezer}</p>
               </div>
               <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
+                <h3 className="card__content-heading">Storing Location</h3>
                 <p className="card__storage">{item.storage}</p>
               </div>
             </div>
@@ -87,28 +85,29 @@ export default function Storage() {
         })}
       </div>
       <div className="card__container">
-        <h2 className="card__heading">Fruits</h2>
+        
         {fruitsItems.map((item) => {
           return (
             <div key={item.id} className="card__content-wrapper">
               {" "}
+              <h2 className="card__heading">Fruits</h2>
               <div className="card__name-wrapper">
                 <h3 className="card__name">{item.name}</h3>
               </div>
               <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
+                <h3 className="card__content-heading">At Freshest</h3>
                 <p className="card__fresh">{item.fresh}</p>
               </div>
               <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
+                <h3 className="card__content-heading">Refrigerator</h3>
                 <p className="card__fridge">{item.fridge}</p>
               </div>
               <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
+                <h3 className="card__content-heading">Freezing</h3>
                 <p className="card__freezer">{item.freezer}</p>
               </div>
               <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
+                <h3 className="card__content-heading">Storing Location</h3>
                 <p className="card__storage">{item.storage}</p>
               </div>
             </div>
@@ -117,28 +116,29 @@ export default function Storage() {
       </div>
 
       <div className="card__container">
-        <h2 className="card__heading">Proteins</h2>
+        
         {meatsItems.map((item) => {
           return (
             <div key={item.id} className="card__content-wrapper">
               {" "}
+              <h2 className="card__heading">Proteins</h2>
               <div className="card__name-wrapper">
                 <h3 className="card__name">{item.name}</h3>
               </div>
               <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
+                <h3 className="card__content-heading">At Freshest</h3>
                 <p className="card__fresh">{item.fresh}</p>
               </div>
               <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
+                <h3 className="card__content-heading">Refrigerator</h3>
                 <p className="card__fridge">{item.fridge}</p>
               </div>
               <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
+                <h3 className="card__content-heading">Freezing</h3>
                 <p className="card__freezer">{item.freezer}</p>
               </div>
               <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
+                <h3 className="card__content-heading">Storing Location</h3>
                 <p className="card__storage">{item.storage}</p>
               </div>
             </div>
@@ -147,28 +147,28 @@ export default function Storage() {
       </div>
 
       <div className="card__container">
-        <h2 className="card__heading">Pantry Items</h2>
         {pantryItems.map((item) => {
           return (
             <div key={item.id} className="card__content-wrapper">
               {" "}
+              <h2 className="card__heading">Pantry Items</h2>
               <div className="card__name-wrapper">
                 <h3 className="card__name">{item.name}</h3>
               </div>
               <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
+                <h3 className="card__content-heading">At Freshest</h3>
                 <p className="card__fresh">{item.fresh}</p>
               </div>
               <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
+                <h3 className="card__content-heading">Refrigerator</h3>
                 <p className="card__fridge">{item.fridge}</p>
               </div>
               <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
+                <h3 className="card__content-heading">Freezing</h3>
                 <p className="card__freezer">{item.freezer}</p>
               </div>
               <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
+                <h3 className="card__content-heading">Storing Location</h3>
                 <p className="card__storage">{item.storage}</p>
               </div>
             </div>
@@ -177,28 +177,28 @@ export default function Storage() {
       </div>
 
       <div className="card__container">
-        <h2 className="card__heading">Condiments, Oils & Spices</h2>
         {condimentsItems.map((item) => {
           return (
             <div key={item.id} className="card__content-wrapper">
               {" "}
+              <h2 className="card__heading">Condiments, Oils & Spices</h2>
               <div className="card__name-wrapper">
                 <h3 className="card__name">{item.name}</h3>
               </div>
               <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
+                <h3 className="card__content-heading">At Freshest</h3>
                 <p className="card__fresh">{item.fresh}</p>
               </div>
               <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
+                <h3 className="card__content-heading">Refrigerator</h3>
                 <p className="card__fridge">{item.fridge}</p>
               </div>
               <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
+                <h3 className="card__content-heading">Freezing</h3>
                 <p className="card__freezer">{item.freezer}</p>
               </div>
               <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
+                <h3 className="card__content-heading">Storing Location</h3>
                 <p className="card__storage">{item.storage}</p>
               </div>
             </div>
@@ -207,28 +207,28 @@ export default function Storage() {
       </div>
 
       <div className="card__container">
-        <h2 className="card__heading">Vegan Proteins</h2>
         {veganProteinsItems.map((item) => {
           return (
             <div key={item.id} className="card__content-wrapper">
               {" "}
+              <h2 className="card__heading">Vegan Proteins</h2>
               <div className="card__name-wrapper">
                 <h3 className="card__name">{item.name}</h3>
               </div>
               <div className="card__fresh-wrapper">
-                <h4 className="card__content-heading">At Freshest</h4>
+                <h3 className="card__content-heading">At Freshest</h3>
                 <p className="card__fresh">{item.fresh}</p>
               </div>
               <div className="card__fridge-wrapper">
-                <h4 className="card__content-heading">Refrigerator</h4>
+                <h3 className="card__content-heading">Refrigerator</h3>
                 <p className="card__fridge">{item.fridge}</p>
               </div>
               <div className="card__freezer-wrapper">
-                <h4 className="card__content-heading">Freezing</h4>
+                <h3 className="card__content-heading">Freezing</h3>
                 <p className="card__freezer">{item.freezer}</p>
               </div>
               <div className="card__storage-wrapper">
-                <h4 className="card__content-heading">Storing Location</h4>
+                <h3 className="card__content-heading">Storing Location</h3>
                 <p className="card__storage">{item.storage}</p>
               </div>
             </div>
